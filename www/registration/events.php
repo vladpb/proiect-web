e68dcf21c413f2b3c2eb4e8d0bf2d1a167612a6b
vladpb <vlad.bejat@gmail.com>
2023-11-15 23:48:31 +0200

    -events page etc

--- www/registration/events.php
+++ www/registration/events.php
@@ -9,7 +9,7 @@ $result = mysqli_query($db, $query);
 <div class="events">
     <?php while ($event = mysqli_fetch_assoc($result)) : ?>
         <div class="event">
-            <h2><?php echo $event['titlu']; ?></h2>
+            <h2><a href="event_page.php?id=<?php echo $event['id']; ?>"><?php echo $event['titlu']; ?></a></h2>
             <p><?php echo $event['descriere']; ?></p>
             <p><?php echo $event['data']; ?> at <?php echo $event['ora']; ?></p>
             <p>Location: <?php echo $event['locatie']; ?></p>
